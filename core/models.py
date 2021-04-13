a5841de10f90aa545f179b0940aa08cce2081eca
Mayur Jain <jainmayur517@gmail.com>
2021-04-14 00:32:19 +0530

    Update models.py
    final1

--- core/models.py
+++ core/models.py
@@ -2,18 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class usertwitx(models.Model):
+class usertwitx(models.Model):    #for user
   user_id=models.AutoField
   user_name=models.CharField(max_length=50)
   tweet_id=models.CharField(max_length=100,unique=True)
   user_c=models.CharField(max_length=50)
   desc= models.TextField(max_length=500)
   url_p=models.TextField(max_length=500)
-#hashtag=models.TextField(max_length=500)
-  # image=models.ImageField()
 
 
-class friendtweet(models.Model):
+
+class friendtweet(models.Model):  #for user following accounts
   friend_id=models.AutoField
   friend_name=models.CharField(max_length=50)
   tweetf_id=models.CharField(max_length=100,unique=True)
